0928b10bfb61960142c33db6d72760309f06df64
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-06-29 20:57:33 -0600

    Building Story Data

---
+++ plot.js
@@ -0,0 +1,75 @@
+// Intro
+'#name# lived in #settingHome#, part of #settingMaster#, but it wasn\'t as exciting as one would think. #name# was but a lowly #lameJob#. In #pronounTheir# dreams, however, #name# was a #sugarMighty# #coolJob# and #sugarWent# across the #settingLand#. But those were just dreams. #name# feared #pronounThey# would never leave #settingHome#, and probably never even see a fabled #xFactor#.'
+
+// world
+settingHome
+settingMaster
+settingLand
+lameJob
+coolJob
+
+// xFactor
+xFactor
+
+// CallToAdventure
+'But adventure, as it often does, came to *name\'s* feet! The #xFactor# #xFactorGroup# was attacking #settingHome#! Despite being just #lameJob#, #name# picked up #pronounTheir# #lameWeapon and fought back! #name# fought #sugarBravely, but it wasn\'t enough, and sadly #settingHome# was pillaged and left in ruins.'
+
+// world
+lameWeapon
+
+// xFactor
+xFactorGroup
+
+// Pick UP Mantle
+'#pickUpMantle#'
+
+// plot
+pickUpMantle
+
+// The Journey
+'Armed with #pronounTheir# new #coolWeapon#, #name# #settingTraveled# #settingTransport#. #pronounTheir# destination? The #xFactor# #sugarLair#, #settingEvilLocation#. It would be a long journey, farther than #name# had ever ventured, but #pronounThey# was filled with #plotMotivation#.'
+
+// world
+coolWeapon
+settingTraveled
+settingTransport
+settingEvilLocation
+
+// plot
+plotMotivation
+
+// Challenge!
+'#plotJourneyChallenge# #name# rested just long enough to regain #pronounTheir# focus, then #pronounThey# continued on!'
+
+// plot
+plotJourneyChallenge // lost, attack, none
+'Luckily the journey was witout incident.'
+
+// Confrontation!
+'It was a long journey, but finally, #name# arrived at the #xFactor# #sugarLair# #settingEvilLocation#. #xFactorLeader# was waiting for #pronounThem#!\n "Soon all of #settingMaster# will be mine!" #xFactorLeader# said, #xFactorTaunt#'
+
+// xFactor
+xFactorTaunt
+
+// Plot Twist!
+'#plotTwist#'
+
+//plot
+plotTwist // secret, betrayed, corrupted, none
+'"Let\'s get right to it, the fighting, I mean," #name# said.'
+
+// Final Battle!
+'"#xFactorLeader#!", #name# #sugarCried# drawing #pronounTheir# #coolWeapon#, "Let us end this now."\n"My thoughts exactly!" #xFactorLeader# #sugarYelled#, drawing their own, #sugarSinister# #evilWeapon#. #plotFight#'
+
+
+//plot
+plotFight // brain, Brawn
+
+// Resolution
+'#plotResolution#'
+
+// plot
+plotResolution
+'#heroWins#. #xFactorLeader# was defeated. #heroWinsEnding#'
+
+'#heroLoses#. #xFactorLeader# laughed as #name# collapsed, exhausted and beaten. #heroLosesEnding#.'
---
+++ story.data.js
@@ -0,0 +1,39 @@
+// worlds
+var kingdom = {
+  settingMaster: ['The Kingdom of Magic'],
+  settingHome: ['The Royal City of Magic-Tania', 'The Village of Forest Glenn', 'The Blight Lands of Rhahguhn'],
+  settingLand: ['realm', 'countryside', 'land'],
+  lamejob: ['stable mucker', 'royal silver polisher', 'poopsmith'],
+  cooljob: ['Knight of the Realm', 'Mighty Wizard', 'Dashing Outlaw']
+}
+
+var space = {
+  settingMaster: ['Space Station X-99'],
+  settingHome: ['Habitation Module 1138', 'LifePod Alpha', 'HomeNode BL33T'],
+  settingLand: ['galaxy', 'interstellar seas','stars'],
+  lamejob: ['Space Janitor', 'HyperDrive Waste Collector', 'Viewport Washer'],
+  cooljob: ['Star Pilot', 'Space Marine', 'Quaser Jumper']
+}
+
+var western = {}
+
+// hero
+name //
+pronounTheir // his/her/their
+pronounThey// he/she/they
+pronounThem
+heroWins
+heroWinsEnding
+heroLoses
+heroLosesEnding
+
+// sugar
+var sugar = {
+  sugarWent
+  sugarMighty
+  sugarBravely
+  sugarLair
+  sugarCried
+  sugarYelled
+  sugarSinister
+}
